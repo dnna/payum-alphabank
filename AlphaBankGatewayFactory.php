6bc6ab7938530a2f90dae29d0d430d336edf99e6
dnna <dnna@dnna.gr>
2018-10-19 20:15:17 +0300

    Improve formatting and remove unused imports

--- AlphaBankGatewayFactory.php
+++ AlphaBankGatewayFactory.php
@@ -1,8 +1,7 @@
 <?php
+
 namespace Dnna\Payum\AlphaBank;
 
-use Dnna\Payum\AlphaBank\Action\AuthorizeAction;
-use Dnna\Payum\AlphaBank\Action\CancelAction;
 use Dnna\Payum\AlphaBank\Action\ConvertPaymentAction;
 use Dnna\Payum\AlphaBank\Action\CaptureAction;
 use Dnna\Payum\AlphaBank\Action\RefundAction;
@@ -16,29 +15,42 @@ class AlphaBankGatewayFactory extends GatewayFactory
     /**
      * {@inheritDoc}
      */
-    protected function populateConfig(ArrayObject $config)
+    protected function populateConfig(ArrayObject $config): void
     {
-        $config->defaults([
-            'payum.factory_name' => 'alphabank',
-            'payum.factory_title' => 'Alpha Bank',
+        $config->defaults(
+            [
+                'payum.factory_name' => 'alphabank',
+                'payum.factory_title' => 'Alpha Bank',
 
-            'payum.template.create_charge' => '@DnnaPayumAlphaBank/Action/create_charge.html.twig',
+                'payum.template.create_charge' => '@DnnaPayumAlphaBank/Action/create_charge.html.twig',
 
-            'payum.action.capture' => new CaptureAction(),
-            'payum.action.refund' => new RefundAction(),
-            'payum.action.create_charge' => function (ArrayObject $config) {
-                if($config['sandbox'] == true) {
-                    $actionUrl = 'https://alpha.test.modirum.com/vpos/shophandlermpi';
-                } else {
-                    $actionUrl = 'https://www.alphaecommerce.gr/vpos/shophandlermpi';
-                }
-                return new CreateChargeAction($config['payum.template.create_charge'], $actionUrl, $config['mid'], $config['sharedSecretKey']);
-            },
-            'payum.action.convert_payment' => function (ArrayObject $config) {
-                return new ConvertPaymentAction($config['sandbox'], $config['useMasterPass'], $config['lang'], $config['cssUrl']);
-            },
-            'payum.action.status' => new StatusAction(),
-        ]);
+                'payum.action.capture' => new CaptureAction(),
+                'payum.action.refund' => new RefundAction(),
+                'payum.action.create_charge' => function (ArrayObject $config) {
+                    if ($config['sandbox'] == true) {
+                        $actionUrl = 'https://alpha.test.modirum.com/vpos/shophandlermpi';
+                    } else {
+                        $actionUrl = 'https://www.alphaecommerce.gr/vpos/shophandlermpi';
+                    }
+
+                    return new CreateChargeAction(
+                        $config['payum.template.create_charge'],
+                        $actionUrl,
+                        $config['mid'],
+                        $config['sharedSecretKey']
+                    );
+                },
+                'payum.action.convert_payment' => function (ArrayObject $config) {
+                    return new ConvertPaymentAction(
+                        $config['sandbox'],
+                        $config['useMasterPass'],
+                        $config['lang'],
+                        $config['cssUrl']
+                    );
+                },
+                'payum.action.status' => new StatusAction(),
+            ]
+        );
 
         if (false == $config['payum.api']) {
             $config['payum.default_options'] = array(
@@ -55,12 +67,15 @@ class AlphaBankGatewayFactory extends GatewayFactory
             $config['payum.api'] = function (ArrayObject $config) {
                 $config->validateNotEmpty($config['payum.required_options']);
 
-                return new Api((array) $config, $config['payum.http_client'], $config['httplug.message_factory']);
+                return new Api((array)$config, $config['payum.http_client'], $config['httplug.message_factory']);
             };
         }
 
-        $config['payum.paths'] = array_replace([
-            'DnnaPayumAlphaBank' => __DIR__.'/Resources/views',
-        ], $config['payum.paths'] ?: []);
+        $config['payum.paths'] = array_replace(
+            [
+                'DnnaPayumAlphaBank' => __DIR__ . '/Resources/views',
+            ],
+            $config['payum.paths'] ?: []
+        );
     }
 }
